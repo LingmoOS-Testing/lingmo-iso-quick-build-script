4779b822c992c5026bb9d96555b9991d8999df6b
Yinan Qin <39023210+ganyuanzhen@users.noreply.github.com>
2024-05-02 04:52:06 +0000

    --include

--- main.sh
+++ main.sh
@@ -42,7 +42,7 @@ echo '------'
 echo "We are going to create base system. Press enter to continue or Ctrl+C to exit."
 
 
-debootstrap --arch=amd64 bookworm ${WORK}/rootfs http://deb.debian.org/debian ca-certificates
+debootstrap --arch=amd64 bookworm ${WORK}/rootfs http://deb.debian.org/debian --include ca-certificates
  
 # Change sources.
 
